ac3d50a91fff640342e734915835c3ce424a1338
John Quigley <johnpeter@quigley.email>
2017-04-16 11:16:46 -0400

    Surprise links, mobile friendly images

---
+++ resources/javascript/popup.js
@@ -0,0 +1,10 @@
+// When the user clicks on <div>, open the popup
+function myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+}
+
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
